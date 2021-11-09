92b8871e494a5c2a7d5b077dc977f84eced85658
Michele Comolli <mikicomolli@gmail.com>
2021-11-09 18:44:27 +0100

    routes defined 👌

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tracks#index'
+  get 'tracks', to: 'tracks#index'
+  get 'tracks/new', to: 'tracks#new', as: :new_track
+  post 'tracks', to: 'tracks#create'
+  get 'tracks/:id', to: 'tracks#show', as: :track
+  get 'tracks/:id/edit', to: 'tracks#edit', as: :edit_track
+  patch 'tracks/:id', to: 'tracks#update'
+  delete 'tracks/:id', to: 'tracks#destroy', as: :destroy_track
 end
